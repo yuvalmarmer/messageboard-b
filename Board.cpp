21870bbdaaf0d3806c77ca7701ebced390b5715f
Yuval Marmer <yuvalmarmer97@gmail.com>
2021-03-27 13:49:05 +0300

    unsignt int

--- Board.cpp
+++ Board.cpp
@@ -30,12 +30,12 @@ std::string Board::read(unsigned int row, unsigned int column, Direction d , uns
 	//Check if in row and cols are in the allocated board
 	if(row+length<rows && column+length<cols){ 
 		if(d==Direction::Horizontal){	
-			for(int i=0;i<length;++i){
+			for(size_t i=0;i<length;++i){
 				str += board[row].at(i);
 			}
 		}
 		else{ //Vertical
-			for(int i=0;i<length;++i){
+			for(size_t i=0;i<length;++i){
 				str += board[i].at(column);
 			}
 		}
