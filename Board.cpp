8c3931fef988548a015c9d004887f39c9d70760f
Yuval Marmer <yuvalmarmer97@gmail.com>
2021-04-01 19:04:41 +0300

    Fixing

--- Board.cpp
+++ Board.cpp
@@ -129,7 +129,7 @@ std::string Board::read(unsigned int row, unsigned int column, Direction d , uns
 void Board::show(){
 
 	for(size_t i = topRowPost;i < bottRowPost; ++i){
-		for (size_t j = leftColPost; j < rightColPost;++j){
+		for (size_t j = leftColPost; j < rightColPost - 1;++j){
 			std::cout << board->at(i)[j];
 		}
 		std::cout << std::endl;
